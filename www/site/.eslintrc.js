662fba9113f0627e83b2f1f4165f1445d723ebcb
Dmitriy <dima.385@mail.ru>
2022-04-07 00:51:21 +0500

    init front

---
+++ www/site/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    browser: true,
+    commonjs: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/base',
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:promise/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['vue', 'prettier', 'promise'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        htmlWhitespaceSensitivity: 'ignore',
+      },
+    ],
+    'promise/always-return': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/catch-or-return': 'error',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'warn',
+    'promise/no-promise-in-callback': 'warn',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/avoid-new': 'warn',
+    'promise/no-new-statics': 'error',
+    'promise/no-return-in-finally': 'warn',
+    'promise/valid-params': 'warn',
+  },
+
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+}
---
+++ www/site/src/router.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+// 1. Define route components.
+// These can be imported from other files
+import IndexPage from '@/pages/index.vue'
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [{ path: '/', component: IndexPage }]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+export default createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
